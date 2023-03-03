06ddf8bd58a38cd49e4b858776373d3476596c2f
jmodestoag <alvarezgomezmodesto@gmail.com>
2023-03-02 17:20:48 -0700

    primer commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+import Icon from 'mod-icon-library'
+
+function App() {
+  return (
+    <div className="App">
+        <Icon name="Ticketplus" />
+       <h>Test App</h>
+     
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/iconos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import {TicketPlus} from 'mod-icon-library'
+
+const Example = () => {
+    return (<TicketPlus />)
+}
+ 
+export default Example;

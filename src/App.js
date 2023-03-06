e202a454d83ae7c4f4da9f10c3012e184d0c3b37
jmodestoag <alvarezgomezmodesto@gmail.com>
2023-03-06 15:12:29 -0700

    Se actualiza libreria de iconos 2.1.9

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-
+import React from 'react';
 import './App.css';
-
 import Icon from 'mod-icon-library'
 
 function App() {
   return (
-    <div className="App">
-        <Icon name="Ticketplus" />
-       <h>Test App</h>
-     
-   
+    <div className="App">      
+       <h1>Iconos SVG Personalizados</h1> 
+
+       <div className='row'>
+           <div className='col'>
+              <Icon name="ticketPlusCircular"/>
+             
+              {/* 
+              <Icon name="IconoChatUnread"/>
+              <Icon name="IconoChatRead"/>
+              <Icon name="IconoNotificaciones"/> */}
+           </div>
+           <div>
+              <Icon name="ticketPlus"/>
+           </div>
+           <div>
+              <Icon name="IconoChatRead"/>
+           </div>
+        <div>
+              <Icon name="IconoChatUnread"/>
+           </div>
+       </div>
+      
     </div>
   );
 }

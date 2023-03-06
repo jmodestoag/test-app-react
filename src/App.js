d2fd31e2dbf2ceafcf70eda1eb09f51d0c2f6a90
jmodestoag <alvarezgomezmodesto@gmail.com>
2023-03-06 16:37:49 -0700

    version 2.1.11

--- src/App.js
+++ src/App.js
@@ -7,28 +7,36 @@ function App() {
     <div className="App">      
        <h1>Iconos SVG Personalizados</h1> 
 
-       <div className='row'>
-           <div className='col'>
-              <Icon name="ticketPlusCircular"/>
-             
-              {/* 
-              <Icon name="IconoChatUnread"/>
-              <Icon name="IconoChatRead"/>
-              <Icon name="IconoNotificaciones"/> */}
-           </div>
-           <div>
-              <Icon name="ticketPlus"/>
-           </div>
-           <div>
-              <Icon name="IconoChatRead"/>
-           </div>
+      <div className='row'>
+        <div className='col'>
+          <Icon name="ticketPlusCircular" />
+        </div>
         <div>
-              <Icon name="IconoChatUnread"/>
-           </div>
-       </div>
+          <Icon name="ticketPlus" />
+        </div>
+        <div>
+          <Icon name="IconoChatRead" />
+        </div>
+        <div>
+          <Icon name="IconoChatUnread" />
+        </div>
+        <div>
+          <Icon name="IconDelete"/>
+        </div>
+      
+      </div>
       
     </div>
   );
 }
 
 export default App;
+
+
+const styles = { 
+  color: 'white',
+  with: '42px',
+  height: '42px'
+}
+
+
